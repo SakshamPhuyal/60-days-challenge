9b8655dcef652fc599d02378a09265fcc3962856
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-12 14:19:06 +0545

    12th day

---
+++ 12th day/12thday.js
@@ -0,0 +1,10 @@
+console.log("Saksham phuyal");
+const student={
+    name:"Saksham Phuyal",
+    age:20,
+    Class:12,
+    GPA:4.0,
+};
+console.log(student);
+console.log(student.name);
+console.log(student.GPA);
