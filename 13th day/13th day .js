66d06d1a8f18ab7bd1bdfb3772f63b86a5ec24a6
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-13 13:06:36 +0545

    12th day

---
+++ 13th day/13th day .js
@@ -0,0 +1,24 @@
+//13th Day of learning with leapfrog
+/*13 days completed
+  out of 60 */
+let a=Number(prompt("Enter first number"));
+let b=Number(prompt("Enter Second number"));
+//Arithmetic operators
+console.log("a=",a ,"b=",b)
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a**b=",a**b);
+console.log("a++=",a++,"a--=",a--);
+//Assignment operators
+console.log("a+=b=",a+=b);//It performs a+b. Same for other operations changin operators
+//Comparison Operator
+console.log("a<b:",a<b);
+console.log("a>b:",a>b);
+console.log("a==b:",a==b);
+console.log("a!=b:",a!=b);
+//logical Operator
+console.log("a&&b:",a&&b);
+console.log("a||b:",a||b);
+console.log("!b=",!b);
