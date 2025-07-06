8e624a0b2c0f8015b117c9c9c4cfee92ca7e559f
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-07-06 21:27:37 +0545

    36th day

---
+++ 36th day/src/Card.jsx
@@ -0,0 +1,38 @@
+import sakshamimg from './assets/saksham.jpg'
+function Card(){
+return(
+    <>
+    <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+    <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+      <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+      <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+      <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+      <div className="card">
+        <img src={sakshamimg}alt="profile-picture"></img>
+        <h1>Saksham Phuyal</h1>
+        <p>Hey! guys I'm saksham Phuyal pursuing Bachelors in CSIt from TU.</p>
+    </div>
+    </>
+);
+}
+export default Card
