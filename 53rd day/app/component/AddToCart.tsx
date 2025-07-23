a636e8da06df4763f6a1d538de07843a649e1072
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-07-23 16:32:09 +0545

    53rd day

--- 53rd day/app/component/AddToCart.tsx
+++ 53rd day/app/component/AddToCart.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 const AddToCart = () => {
   return (
-    <div>
-      <button className='h-20 w-30 pointer:cursor rounded-xl bg-amber-300 border-2' onClick={()=>console.log("click me.")}>Add to cart</button>
-    </div>
+<div>
+<button className='h-20 w-30 pointer:cursor rounded-xl bg-amber-300 border-2' 
+onClick={()=>console.log("click me.")}>
+Add to cart
+</button>
+</div>
   );
 };
 
--- 53rd day/app/users/page.tsx
+++ 53rd day/app/users/page.tsx
@@ -10,11 +10,12 @@ const Userspage = async() => {
   return (
   <div className="min-h-screen bg-black text-white p-5">
 
-      <h1 className='text-2xl font-bold'>Users</h1>
-
-      <ul>
-        {users.map(user=>(<li key={user.id}>{user.name}</li>))}
-      </ul>
+      <h1 className='text-2xl font-bold'>
+        Users
+      </h1>
+<ul>
+{users.map(user=>(<li key={user.id}>{user.name}</li>))}
+</ul>
     </div>
   )
 }
