923a6c2d001e81d14107931f155da4b276f7b912
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-07-24 18:14:18 +0545

    54th day

--- 53rd day/app/component/ProductCard.tsx
+++ 53rd day/app/component/ProductCard.tsx
@@ -4,8 +4,12 @@ import AddToCart from './AddToCart';
 const ProductCard = () => {
   return (
     <div className="w-160 p-4 border rounded shadow-md bg-sky-300 text-xl mb-4 hover:bg-sky-600">
-      <h2 className="font-semibold text-lg mb-2">Product Name</h2>
-      <p className="mb-1">Product description goes here.</p>
+      <h2 className="font-semibold text-lg mb-2">
+        Product Name
+        </h2>
+      <p className="mb-1">
+        Product description goes here.
+      </p>
       <AddToCart />
     </div>
   );
