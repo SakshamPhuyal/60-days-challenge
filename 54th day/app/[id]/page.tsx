2ad353eaf0dc9488a4dc154a5c713d9c9870816b
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-07-25 14:14:39 +0545

    Error page

--- 54th day/app/[id]/page.tsx
+++ 54th day/app/[id]/page.tsx
@@ -5,9 +5,9 @@ interface props{
 const detailpage = ({params:{id}}:props) => {
   return (
     <div  className="flex flex-col justify-center items-center bg-gray-700 text-white min-h-screen text-3xl">
-              <p className="text-4xl font-extrabold">#60DaysOfLearning2025</p>
+    <p className="text-4xl font-extrabold">#60DaysOfLearning2025</p>
     <p className="text-4xl font-extrabold mb-5">Day 54</p>
-      userdetails {id}
+    userdetails {id}
     </div>
   )
 }
--- 54th day/app/page.tsx
+++ 54th day/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-700 text-white min-h-screen text-3xl">
-        <p className="text-4xl font-extrabold">#60DaysOfLearning2025</p>
-    <p className="text-4xl font-extrabold mb-5">Day 54</p>
-      <p className="text-5xl">Time:  {new Date().toLocaleTimeString()}</p>
-      </div>
+  <div className="flex flex-col justify-center items-center bg-gray-700 text-white min-h-screen text-3xl">
+  <p className="text-4xl font-extrabold">#60DaysOfLearning2025</p>
+  <p className="text-4xl font-extrabold mb-5">Day 54</p>
+  <p className="text-5xl">Time:  {new Date().toLocaleTimeString()}</p>
+  </div>
   );
 }
